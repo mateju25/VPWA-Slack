4501dbab618b9eaf11c4933c44dff8dec7143ba7
Matej Delincak <55759748+mateju25@users.noreply.github.com>
2022-03-02 09:47:54 +0100

    added infinite scroll with chat component
    ale neviem ci moze byt

--- slack/src/pages/Index.vue
+++ slack/src/pages/Index.vue
@@ -1,17 +1,130 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    />
+  <q-page>
+    <div class="q-pa-md column almost-window-height content-between">
+      <q-infinite-scroll @load="onLoad" class="full-width chat overflow-auto" reverse>
+        <template v-slot:loading>
+          <div class="row justify-center q-my-md">
+            <q-spinner color="primary" name="dots" size="40px" />
+          </div>
+        </template>
+
+        <q-chat-message
+          label="Sunday, 19th"
+        />
+
+        <q-chat-message
+          name="me"
+          avatar="https://cdn.quasar.dev/img/avatar4.jpg"
+          :text="['hey, how are you?']"
+          sent
+          stamp="7 minutes ago"
+        />
+        <q-chat-message
+          name="Jane"
+          avatar="https://cdn.quasar.dev/img/avatar3.jpg"
+          :text="['doing fine, how r you?']"
+          stamp="4 minutes ago"
+        />
+        <q-chat-message
+          name="me"
+          avatar="https://cdn.quasar.dev/img/avatar4.jpg"
+          :text="['hey, how are you?']"
+          sent
+          stamp="7 minutes ago"
+        />
+        <q-chat-message
+          name="Jane"
+          avatar="https://cdn.quasar.dev/img/avatar3.jpg"
+          :text="['doing fine, how r you?']"
+          stamp="4 minutes ago"
+        />
+        <q-chat-message
+          name="me"
+          avatar="https://cdn.quasar.dev/img/avatar4.jpg"
+          :text="['hey, how are you?']"
+          sent
+          stamp="7 minutes ago"
+        />
+        <q-chat-message
+          name="Jane"
+          avatar="https://cdn.quasar.dev/img/avatar3.jpg"
+          :text="['doing fine, how r you?']"
+          stamp="4 minutes ago"
+        />
+        <q-chat-message
+          name="me"
+          avatar="https://cdn.quasar.dev/img/avatar4.jpg"
+          :text="['hey, how are you?']"
+          sent
+          stamp="7 minutes ago"
+        />
+        <q-chat-message
+          name="Jane"
+          avatar="https://cdn.quasar.dev/img/avatar3.jpg"
+          :text="['doing fine, how r you?']"
+          stamp="4 minutes ago"
+        />
+        <q-chat-message
+          name="me"
+          avatar="https://cdn.quasar.dev/img/avatar4.jpg"
+          :text="['hey, how are you?']"
+          sent
+          stamp="7 minutes ago"
+        />
+        <q-chat-message
+          name="Jane"
+          avatar="https://cdn.quasar.dev/img/avatar3.jpg"
+          :text="['doing fine, how r you?']"
+          stamp="4 minutes ago"
+        />
+        <q-chat-message
+          name="me"
+          avatar="https://cdn.quasar.dev/img/avatar4.jpg"
+          :text="['hey, how are you?']"
+          sent
+          stamp="7 minutes ago"
+        />
+        <q-chat-message
+          name="Jane"
+          avatar="https://cdn.quasar.dev/img/avatar3.jpg"
+          :text="['doing fine, how r you?']"
+          stamp="4 minutes ago"
+        />
+      </q-infinite-scroll>
+      <div class="full-width q-mt-sm">
+        <q-form
+          @submit="onSubmit"
+          @reset="onReset"
+          class="q-gutter-md"
+        >
+          <q-input
+            filled
+            v-model="name"
+            placeholder="Message"
+          />
+
+          <div>
+            <q-btn label="Submit" type="submit" color="primary"/>
+          </div>
+        </q-form>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import {defineComponent} from "vue";
 
 export default defineComponent({
   name: "PageIndex",
 });
 </script>
+
+<style>
+.almost-window-height {
+  height: 94vh !important;
+}
+.chat {
+  height: 78vh;
+}
+</style>
