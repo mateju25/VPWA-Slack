d1360cfe58564a51eb9fe05bb10b3175d1c9b1f6
luciaondovcik <55868872+luciaondovcik@users.noreply.github.com>
2022-03-06 21:27:35 +0100

    mobile avatar, dark-mode

--- slack/src/layouts/MainLayout.vue
+++ slack/src/layouts/MainLayout.vue
@@ -3,21 +3,81 @@
 
     <q-header elevated class="bg-primary text-white">
       <q-toolbar class="content-center">
-        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+        <q-btn dense flat round icon="menu" class="hide-channels" @click="toggleLeftDrawer" />
 
         <q-toolbar-title class="flex flex-center">
-          <p class="heading">Title</p>
+          <p class="heading">VoidMessenger</p>
         </q-toolbar-title>
 
-        <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
+        <q-btn dense flat round icon="people" class="hide-users" @click="toggleRightDrawer" />
+        
+        <q-btn
+          dense
+          flat
+          round
+          class="hide-avatar"
+        >
+          <q-avatar 
+            color="blue-grey-11" 
+            square
+            text-color="primary"
+            size="36px"
+          >
+            {{ user.nick_name[0] }}
+            <q-badge
+              class="absolute-bottom-right badge-state"
+              :class="userState(user.state)"
+            />
+          </q-avatar>
+        </q-btn>
+
       </q-toolbar>
     </q-header>
 
-    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
+    <q-drawer
+      show-if-above
+      v-model="leftDrawerOpen"
+      side="left"
+      :breakpoint="768"
+      bordered
+    >
       <ChannelList/>
+      
+      <div
+        class="absolute-bottom-left mobile-avatar"
+      >
+        <q-separator/>
+        <q-btn
+          dense
+          class="q-ma-sm"
+        >
+          <q-avatar 
+            color="blue-grey-11" 
+            square
+            text-color="primary"
+            size="36px"
+            class="q-mr-sm"
+          >
+            {{ user.nick_name[0] }}
+            <q-badge
+              class="absolute-bottom-right badge-state"
+              :class="userState(user.state)"
+            />
+          </q-avatar>
+          {{ user.nick_name }}
+        </q-btn>
+      </div>      
+
     </q-drawer>
 
-    <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
+    <q-drawer
+      show-if-above
+      v-model="rightDrawerOpen"
+      side="right"
+      :breakpoint="992"
+      icon="people"
+      bordered
+    >
       <UserContactList/>
     </q-drawer>
 
@@ -28,7 +88,7 @@
   </q-layout>
 </template>
 
-<script>
+<script lang="ts">
 import { defineComponent, ref } from 'vue';
 import UserContactList from 'src/components/UserContactList.vue';
 import ChannelList from 'src/components/ChannelList.vue';
@@ -38,6 +98,12 @@ export default defineComponent({
   components: {
     UserContactList,
     ChannelList
+  },
+
+  data() {
+    return {
+      user: { 'id': 1, 'nick_name': 'Jesse', 'state': 'Online' },
+    }
   },
 
   setup () {
@@ -54,6 +120,24 @@ export default defineComponent({
       }
     }
   },
+
+  methods: {
+    userState(state: string): string {
+      let color = 'bg-negative';
+      switch(state){
+        case 'Online':
+          color='bg-positive';
+          break;
+        case 'Offline':
+          color='bg-negative';
+          break;
+        case 'DND':
+          color='bg-warning';
+          break;
+      };
+      return color;
+    },
+  },
 })
 </script>
 
@@ -61,4 +145,27 @@ export default defineComponent({
 header div .heading {
   margin: 0;
 }
+
+@media (min-width: 992px) {
+  .hide-users, .mobile-avatar{
+    display: none;
+  }
+}
+
+@media (max-width: 992px) {
+  .hide-avatar{
+    display: none;
+  }
+}
+
+@media (min-width: 768px) {
+  .hide-channels{
+    display: none;
+  }
+}
+
+.mobile-avatar{
+  width:100%;
+}
+
 </style>
