8cd0de952d1f79403883ab8a1ae1eb451f3ff23b
Matej Delincak <55759748+mateju25@users.noreply.github.com>
2022-04-02 17:21:35 +0200

    fix

--- backend-adonis/database/seeders/User.ts
+++ backend-adonis/database/seeders/User.ts
@@ -1,6 +1,5 @@
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder';
 import User from 'App/Models/User';
-import State from 'App/Models/State';
 import argon2 from 'argon2';
 
 export default class UserSeeder extends BaseSeeder {
@@ -8,46 +7,42 @@ export default class UserSeeder extends BaseSeeder {
     // Write your database queries inside the run method
     const uniqueKey = 'username';
 
-    let states = await State.all();
-    let offline = states.find((state) => state.id === 2) as State;
-
-    let users = await User.updateOrCreateMany(uniqueKey, [
+    await User.updateOrCreateMany(uniqueKey, [
       {
         email: 'jesse@jones.com',
         username: 'jesse',
         fullname: 'Jesse Jones',
+        state_id: 2,
         password: await argon2.hash('password'),
       },
       {
         email: 'john@jones.com',
         username: 'john',
         fullname: 'John Jones',
+        state_id: 2,
         password: await argon2.hash('password'),
       },
       {
         email: 'tina@jones.com',
         username: 'tina',
         fullname: 'Tina Jones',
+        state_id: 2,
         password: await argon2.hash('password'),
       },
       {
         email: 'clarence@jones.com',
         username: 'clarence',
         fullname: 'Clarence Jones',
+        state_id: 2,
         password: await argon2.hash('password'),
       },
       {
         email: 'anne@jones.com',
         username: 'anne',
         fullname: 'Anne Jones',
+        state_id: 2,
         password: await argon2.hash('password'),
       },
     ]);
-    users.forEach((user) => {
-      user.state_id = offline.id;
-    });
-    for (let user of users) {
-      await user.save();
-    }
   }
 }
