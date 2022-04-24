e7bf36fd67d524938ec6b780f0a83f6a5c342316
Matej Delincak <55759748+mateju25@users.noreply.github.com>
2022-04-24 16:20:44 +0200

    docker

--- backend-adonis/database/seeders/MainSeeder/Index.ts
+++ backend-adonis/database/seeders/MainSeeder/Index.ts
@@ -1,5 +1,6 @@
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder';
 import Application from '@ioc:Adonis/Core/Application';
+import User from 'App/Models/User';
 
 export default class IndexSeeder extends BaseSeeder {
   private async runSeeder(seeder: { default: typeof BaseSeeder }) {
@@ -15,10 +16,14 @@ export default class IndexSeeder extends BaseSeeder {
   }
 
   public async run() {
-    await this.runSeeder(await import('../Role'));
-    await this.runSeeder(await import('../User'));
-    await this.runSeeder(await import('../Channel'));
-    await this.runSeeder(await import('../Message'));
-    await this.runSeeder(await import('../UnreadChannel'));
+    const users = await User.all();
+    console.log(users.length);
+    if (users.length === 0) {
+      await this.runSeeder(await import('../Role'));
+      await this.runSeeder(await import('../User'));
+      await this.runSeeder(await import('../Channel'));
+      await this.runSeeder(await import('../Message'));
+      await this.runSeeder(await import('../UnreadChannel'));
+    }
   }
 }
